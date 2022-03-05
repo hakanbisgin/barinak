5a47dc161f65484854eda7ed0d1179445f38a9d0
hakanbisgin <hakanbisgin@aof.anadolu.edu.tr>
2022-03-05 21:16:52 +0300

    Add scripts and style yields layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -15,6 +15,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
             crossorigin="anonymous"></script>
+    @yield('style')
+    @yield('head-scripts')
 
 </head>
 <body>
@@ -181,5 +183,6 @@
         </div>
     </footer>
 </div>
+@yield('bottom-scripts')
 </body>
 </html>
